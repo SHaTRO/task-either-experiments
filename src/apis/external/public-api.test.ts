19ed306717ca0628b167e8422f857a438bd824de
Steve Harris <dnsadm@aetrix.com>
2022-06-09 23:50:04 -0700

    fix: improve test performance
    Add fixtures and nocks for api tests. Switch to @swc/jest from ts-jest.

--- src/apis/external/public-api.test.ts
+++ src/apis/external/public-api.test.ts
@@ -1,10 +1,20 @@
 import { fetchCategoriesTE, fetchEntriesTE } from "./public-api";
 import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
+import nock from 'nock';
+import { readFileSync } from 'fs';
+import * as path from 'path';
+
+import { config } from '../../config';
+
 
 describe('Public APIs Database', () => {
 
   it('fetch categories', async () => {
+    const contents = JSON.parse(readFileSync(path.resolve(__dirname, '../../../fixtures/public-api-categories.txt'), 'utf-8'));
+    nock(config.public_apis.url)
+      .get(config.public_apis.paths.categories)
+      .reply(200, contents);
     const { count, categories } = pipe(
       await fetchCategoriesTE(),
       E.getOrElseW( (e) => { throw e }),
@@ -15,6 +25,10 @@ describe('Public APIs Database', () => {
   });
 
   it('fetch entries', async () => {
+    const contents = JSON.parse(readFileSync(path.resolve(__dirname, '../../../fixtures/public-api-entries.txt'), 'utf-8'));
+    nock(config.public_apis.url)
+      .get(config.public_apis.paths.entries)
+      .reply(200, contents);
     const { count, entries } = pipe(
       await fetchEntriesTE(),
       E.getOrElseW( (e) => { throw e }),
